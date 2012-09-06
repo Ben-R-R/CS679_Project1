0a7eba3ebc71c1200ce90f98b0b9cf2f3658eb4e
Mark Dhillon <mark.dhillon@gmail.com>
2012-09-05 23:45:16 -0500

    Phase 10 - Click to add new ball (complete phase 1)

--- tutor.js
+++ tutor.js
@@ -10,6 +10,7 @@
 // phase 7 - objectness
 // phase 8 - object creation
 // phase 9 - multiple objects
+// phase 10 - click to add
 
 // note: I am choosing to do this as an "onload" function for the
 // window (so it gets run when the window is done loading), rather 
@@ -45,7 +46,7 @@ window.onload = function() {
     // this is a slightly weird way to make an object, but it's very
     // javascripty
     var aBall = {
-        "r" : 50,
+        "r" : 10,
         "x" : 100,
         "y" : 100,
         "vX" : 20,
@@ -116,13 +117,16 @@ window.onload = function() {
        }
     }
      
-    // note that we cannot "loop" the following code will just hang:
-    //while (1) {
-    //  drawBall();
-    //  moveBall();
-    //}
-    // don't even try it
-     
+    // what to do when things get clicked
+    function doClick(evt){
+        // a catch - we need to adjust for where the canvas is!
+        // this is quite ugly without some degree of support from
+        // a library
+        theBalls.push( makeBall(evt.pageX - theCanvas.offsetLeft,
+        evt.pageY - theCanvas.offsetTop) );
+    }
+    theCanvas.addEventListener("click",doClick,false);
+
     // what we need to do is define a function that updates the position
     // draws, then schedules another iteration in the future
     // WARNING: this is the simplest, but not the best, way to do this
